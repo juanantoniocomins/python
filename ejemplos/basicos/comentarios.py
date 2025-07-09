593fcb2a47c1be83d0df375dde645272b85bb685
JuanComins <89415337+Angeljuan666@users.noreply.github.com>
2025-07-09 13:42:52 +0200

    Add files via upload

---
+++ ejemplos/basicos/comentarios.py
@@ -0,0 +1,19 @@
+# comentario de una sola linea
+
+print("Prueba1") # esta linea imprime la palabra prueba
+
+# comentarios de varias lineas: ''' o """
+
+'''
+Esto es
+un comentario
+de  varias lineas
+'''
+
+"""
+Esto es
+un comentario
+de  varias lineas
+"""
+
+print("Prueba2") 
