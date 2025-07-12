4142165e77211dfec8f382e92af22cb1bf60fef5
JuanComins <89415337+Angeljuan666@users.noreply.github.com>
2025-07-13 00:42:37 +0200

    Add files via upload

---
+++ ejemplos/listas/listas/convertir_string_a_lista.py
@@ -0,0 +1,13 @@
+# split(sep)	
+# Dividir un string en una lista, usando un separador que tú eliges.
+
+# Sin parámetro (divide por espacios)
+
+frase = "Hola Juanan ¿cómo estás?"
+lista = frase.split()
+print(lista)
+
+
+
+
+
