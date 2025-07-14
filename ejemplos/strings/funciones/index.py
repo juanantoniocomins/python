02f41d5b65e9ae83601479517e251ccbfca4cbfa
JuanComins <89415337+juanantoniocomins@users.noreply.github.com>
2025-07-14 21:36:27 +0200

    Add files via upload

---
+++ ejemplos/strings/funciones/index.py
@@ -0,0 +1,9 @@
+# Igual que find pero lanza error si no encuentra
+
+cadena = "hola"
+
+# existe = cadena.index("e")
+# print(existe) # no existe: -1
+
+existe = cadena.index("l")
+print(existe) # si existe: devuleve indice
