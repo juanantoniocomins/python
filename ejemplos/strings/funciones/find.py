dfec294c0fbabba3112cb7f6b146824f4abe3389
JuanComins <89415337+juanantoniocomins@users.noreply.github.com>
2025-07-14 21:35:42 +0200

    Add files via upload

---
+++ ejemplos/strings/funciones/find.py
@@ -0,0 +1,10 @@
+# Busca substring, devuelve índice o -1
+
+cadena = "hola"
+
+existe = cadena.find("e")
+print(existe) # no existe: -1
+
+existe = cadena.find("l")
+print(existe) # si existe: devuleve indice
+
