5d698dde719c27df70568f84695035477535273b
JuanComins <89415337+juanantoniocomins@users.noreply.github.com>
2025-07-17 12:29:29 +0200

    Add files via upload

---
+++ ejemplos/control_flujo/basicos/mayor_de_dos_numeros.py
@@ -0,0 +1,7 @@
+num1 = int(input("Introduce el primer número: "))
+num2 = int(input("Intropduce el segundo número: "))
+
+if num1 > num2:
+    print(f"{num1} es mayor que {num2}")
+else:
+    print(f"{num2} es mayor que {num1}")
