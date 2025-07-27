27db801d415643af5c944fc67331ddaf3ab4f8ce
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-07-27 23:51:21 +0200

    Add files via upload

---
+++ ejemplos/strings/ejercicios/cambiar_espacios_blanco.py
@@ -0,0 +1,7 @@
+cadena = input("Introduce una frase: ")
+reemplazar = input("Introduce algo para reemplazar los espacios en blanco: ")
+
+nueva_cadena = cadena.replace(" ", reemplazar)
+
+print(nueva_cadena)
+
