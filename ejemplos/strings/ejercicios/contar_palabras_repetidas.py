c3cd22db95358344961e48ee50b78c58dc281449
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-07-27 23:55:51 +0200

    Add files via upload

---
+++ ejemplos/strings/ejercicios/contar_palabras_repetidas.py
@@ -0,0 +1,6 @@
+cadena = "La casa de mi abuela es una casa antigua, pero me encanta esa casa."
+palabra = "casa"
+nueva_cadena = cadena.lower()
+contador = nueva_cadena.count(palabra)
+
+print(f"{cadena} \nTiene {contador} veces la palabra {palabra}")
