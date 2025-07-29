a69908faf611d21c64a219a4e5b6533d72cb80d2
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-07-29 20:50:12 +0200

    Add files via upload

---
+++ ejemplos/control_flujo/if-else.py
@@ -0,0 +1,13 @@
+# if condición:
+    # bloque si la condición es verdadera
+# else:
+    # bloque si la condición es falsa
+
+# Recuerda: la indentación (sangría) debe ser consistente (4 espacios por nivel).
+
+numero = int(input("Introduce un número: "))
+
+if numero % 2 == 0:
+    print("El número es par.")
+else:
+    print("El número es impar.")
