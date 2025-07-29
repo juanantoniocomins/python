0abb57a6ba5e135b1a23c2b30872c74292ac222c
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-07-29 20:49:50 +0200

    Add files via upload

---
+++ ejemplos/control_flujo/if.py
@@ -0,0 +1,9 @@
+# if condición:
+    # bloque de código si la condición es verdadera
+
+# Importante: La indentación (sangría) es obligatoria en Python. Por convención, se usan 4 espacios.
+
+edad = int(input("Introduce tu edad: "))
+
+if edad >= 18:
+    print("Eres mayor de edad.")
