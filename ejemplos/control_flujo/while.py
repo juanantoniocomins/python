1c6fae97c6963e0fa1316383b91232b8553a03d6
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-07-29 20:51:57 +0200

    Add files via upload

---
+++ ejemplos/control_flujo/while.py
@@ -0,0 +1,11 @@
+# contador (utilizar para salir del bucle))
+
+# while condición:
+        # Bloque de código que se ejecuta mientras la condición sea True
+#       instrucciones
+
+contador = 0
+
+while contador < 5:
+    print("Contador vale:", contador)
+    contador += 1
