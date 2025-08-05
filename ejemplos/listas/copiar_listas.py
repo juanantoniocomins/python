0671bd9d98a23b42460cce1fe7118f7e7a9518d4
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-08-05 22:24:40 +0200

    Add files via upload

---
+++ ejemplos/listas/copiar_listas.py
@@ -0,0 +1,32 @@
+oficios = ['Arquitecto', 'Soldador', 'Montador', 'Arquitecto', 'Pintor', 'Albañil', 'Metal', 'Fontanero']
+nombres = ["Ana", "Carlos", "Beatriz", "David", "Elena", "Fernando", "Gloria", "Héctor", "Irene", "Javier"]
+
+# concatenar
+
+oficios_nombres = oficios + nombres
+
+print(oficios_nombres)
+
+# slicing
+
+lista = oficios[0:4] # inicio a final -1
+
+print(lista)
+
+# metodo copy
+
+nueva = oficios.copy()
+
+print(nueva)
+
+# meotodo list
+
+nueva_lista = list(nueva)
+
+print(nueva_lista)
+
+# metodo zip
+
+union = zip(oficios, nombres)
+
+print(list(union))
