6a991958d7de2865384a089837af528b441e7bbf
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-08-06 20:46:31 +0200

    Add files via upload

---
+++ ejemplos/listas/crear_listas.py
@@ -0,0 +1,13 @@
+mi_lista = ["Juan", "Pedro", "Ana"]
+mi_lista2 = ['Juan', 'Pedro', 'Ana']
+mi_lista3 = [] # se puede crear vacia
+alumno1 = ["Juan", 15, True] # se pueden mezclar los tipos
+
+print(mi_lista)
+print(mi_lista2)
+print(mi_lista3)
+print(alumno1)
+
+mi_lista = ["Elisa"] # machacamos
+
+print(mi_lista)
