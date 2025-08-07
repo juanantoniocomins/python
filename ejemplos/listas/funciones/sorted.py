7997b4e53232c478b21be60da7fed9b79192521b
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-08-08 00:00:16 +0200

    Add files via upload

---
+++ ejemplos/listas/funciones/sorted.py
@@ -0,0 +1,10 @@
+# Devuelve una nueva lista ordenada (sin modificar la original).
+
+lista = [
+    5, 12, 7, 23, 1,
+    18, 9, 30, 4, 15,
+    22, 27, 14, 6, 11,
+    12, 5, 28, 3, 20
+]
+
+print(sorted(lista))
