d26499f0f5ff94c28b441c33fc9c254cd05de842
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-08-09 20:15:02 +0200

    Add files via upload

---
+++ ejemplos/funciones/ejercicios/hola_mundo.py
@@ -0,0 +1,11 @@
+def saludo1():
+    print("Hola mundo1!")
+
+saludo1()
+
+# --------------------------------------------------------------------------------------
+
+def saludos2(primer, segundo): 
+    print(f"{primer}{segundo}")
+
+saludos2("Hola ", "mundo2!")
