8a05e493a82ffeb119c3705b60d8201ad771db32
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-08-23 00:42:43 +0200

    Add files via upload

---
+++ ejemplos/_WINDOWS_/win_listar_directorio4.py
@@ -0,0 +1,22 @@
+import os
+import re
+
+usuario = "juan"
+directorio = "C:\\Users\\" + usuario + "\\Desktop11"
+
+if not os.path.exists(directorio):
+    print(f"⚠️  El directorio {directorio} no existe.")
+else:
+    contador_fich = 0
+    contador_dir = 0
+
+    for i, archivo in enumerate(os.listdir(directorio), start=1):
+        patron = re.compile(r".*\.\w+$", re.IGNORECASE)  # << extensión de 1 o más caracteres
+        
+        if patron.match(archivo):
+            contador_fich += 1
+        else: 
+            contador_dir += 1
+
+    print("")
+    print(f"Hay {contador_fich} ficheros y {contador_dir} directorios en {directorio}")
