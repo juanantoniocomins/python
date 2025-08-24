5f14187d3d09e421026560a66b732da031a3c161
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-08-24 23:52:53 +0200

    Add files via upload

---
+++ ejemplos/calculadora2.py
@@ -0,0 +1,15 @@
+"""
+Pide dos numeros
+El programa sumará, restará y multiplicará dichos números
+"""
+
+n1 = float(input("Introduce el primer número: "))
+n2 = float(input("Introduce el segundo número: "))
+
+suma = n1 + n2
+resta = n1 - n1
+multi = n1 * n2
+
+print (str(n1) + " + " + str(n2) + " = " + str(n1+n2))
+print (str(n1) + " - " + str(n2) + " = " + str(n1-n2))
+print (str(n1) + " * " + str(n2) + " = " + str(n1*n2))
